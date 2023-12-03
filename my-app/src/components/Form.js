8616733e776e19c30f457e5eeef8fd4fd24e8486
kyleung1 <leungkyle2@gmail.com>
2023-12-03 02:08:22 -0800

    mabox

--- my-app/src/components/Form.js
+++ my-app/src/components/Form.js
@@ -1,80 +1,113 @@
-import React, {useState} from 'react'; 
-import {Form, Col } from 'react-bootstrap'; 
-import Row from 'react-bootstrap/row'; 
-import Container from 'react-bootstrap/container'; 
-import Button from 'react-bootstrap/button'; 
+import React, { useState } from "react";
+import { Form, Col } from "react-bootstrap";
+import Row from "react-bootstrap/row";
+import Container from "react-bootstrap/container";
+import Button from "react-bootstrap/button";
+import { useNavigate } from "react-router-dom";
 
 const FormPage = () => {
-    const [fields, setFields] = useState([
+  const [fields, setFields] = useState([
     //     {
-    //     // id: 1, 
+    //     // id: 1,
     //     // task: ""
     // }
-])
+  ]);
 
-    const handleChangeInput = (i, e) => {
-        console.log(e.target.value); 
-        const values = [...fields]
-        values[i][e.target.name] = e.target.value
-        setFields(values)
-    }
+  const handleChangeInput = (i, e) => {
+    console.log(e.target.value);
+    const values = [...fields];
+    values[i][e.target.name] = e.target.value;
+    setFields(values);
+  };
 
-    const handleAdd = (id) => {
-        setFields([...fields, {id: id + 2, task: ""}])
-    }
+  const handleAdd = (id) => {
+    setFields([...fields, { id: id + 2, task: "" }]);
+  };
 
-    const handleSubtract = (i) => {
-        const values = [...fields]
-        values.splice(i, 1)
-        setFields([...values])
-    }
+  const handleSubtract = (i) => {
+    const values = [...fields];
+    values.splice(i, 1);
+    setFields([...values]);
+  };
 
-    return(
-        <div>
-            <Container>
-                <Row style={{fontWeight: "bold", paddingLeft: "100px", paddingRight: "100px"}}>
-                    {/* Task List */}
-                    {fields.length ? 
-                    <Form className="no-bullet">
-                        <Form.Group className="mb-3" controlId="formBasicEntry">
-                            {fields.map((field, i) => (
-                                <div key={field.id}>
-                            <Row className="align-items-center mt-5">
-                                <Col md>
-                                    {/* <Form.Label>First Task</Form.Label> */}
-                                    <Form.Control 
-                                        type="text" 
-                                        placeholder="Enter an errand ..." 
-                                        name="firstErrand"
-                                        value={field.firstErrand}
-                                        onChange={e => handleChangeInput(i, e)}
-                                        />
+  let navigate = useNavigate();
+  const routeChange = () => {
+    let path = "/Mapbox";
+    navigate(path);
+  };
 
-                                </Col>
-                                <Col md>
-                                    <Button disabled={fields.length === 4} onClick={() => handleAdd(i)} className="mt-4 mr-5" style={{float: "center"}}>
-                                        <i className="fas fa-plus"></i>
-                                    </Button>
-                                    <Button onClick={() => handleSubtract(i)}className="minus-button mt-4">
-                                        <i className="fas fa-minus"></i>
-                                    </Button>
-                                </Col>
-                            </Row>
-                            </div>
-                            ))}
-                        </Form.Group> 
-                        <div style={{float: "right", marginRight: "250px"}}>
-                            <Button type="submit" variant="light">
-                                Submit
-                            </Button>
-                        </div>
-                    </Form> : <button style={{width:"250px", marginLeft: "353px", marginTop: "50px"}}className='glow-on-hover' onClick={() => handleAdd(0)}>
-                        Let's Get Started!
-                    </button>}
-                </Row>
-            </Container>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Container>
+        <Row
+          style={{
+            fontWeight: "bold",
+            paddingLeft: "100px",
+            paddingRight: "100px",
+          }}
+        >
+          {/* Task List */}
+          {fields.length ? (
+            <Form className="no-bullet">
+              <Form.Group className="mb-3" controlId="formBasicEntry">
+                {fields.map((field, i) => (
+                  <div key={field.id}>
+                    <Row className="align-items-center mt-5">
+                      <Col md>
+                        {/* <Form.Label>First Task</Form.Label> */}
+                        <Form.Control
+                          type="text"
+                          placeholder="Enter an errand ..."
+                          name="firstErrand"
+                          value={field.firstErrand}
+                          onChange={(e) => handleChangeInput(i, e)}
+                        />
+                      </Col>
+                      <Col md>
+                        <Button
+                          disabled={fields.length === 4}
+                          onClick={() => handleAdd(i)}
+                          className="mt-4 mr-5"
+                          style={{ float: "center" }}
+                        >
+                          <i className="fas fa-plus"></i>
+                        </Button>
+                        <Button
+                          onClick={() => handleSubtract(i)}
+                          className="minus-button mt-4"
+                        >
+                          <i className="fas fa-minus"></i>
+                        </Button>
+                      </Col>
+                    </Row>
+                  </div>
+                ))}
+              </Form.Group>
+              <div style={{ float: "right", marginRight: "250px" }}>
+                <Button
+                  type="submit"
+                  variant="light"
+                  onClick={() => {
+                    routeChange();
+                  }}
+                >
+                  Submit
+                </Button>
+              </div>
+            </Form>
+          ) : (
+            <button
+              style={{ width: "250px", marginLeft: "353px", marginTop: "50px" }}
+              className="glow-on-hover"
+              onClick={() => handleAdd(0)}
+            >
+              Let's Get Started!
+            </button>
+          )}
+        </Row>
+      </Container>
+    </div>
+  );
+};
 
 export default FormPage;
---
+++ my-app/src/components/Mapbox.js
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+
+const Mapbox = (props) => {
+  const Map = ReactMapboxGl({
+    accessToken:
+      "pk.eyJ1IjoiZGltZW50aW8iLCJhIjoiY2xhMngzZmEyMDRtdDN2bW93MjYyY2hvbSJ9.lBP2u-C8BEgug7_ye16y2g",
+  });
+
+  return (
+    <div className="App">
+      <head>
+        <link
+          href="https://api.mapbox.com/mapbox-gl-js/v1.10.1/mapbox-gl.css"
+          rel="stylesheet"
+        />
+      </head>
+      <Map
+        style="mapbox://styles/mapbox/streets-v9"
+        containerStyle={{
+          height: "100vh",
+          width: "100vw",
+        }}
+      >
+        <Layer type="symbol" id="marker" layout={{ "icon-image": "marker-15" }}>
+          <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
+        </Layer>
+      </Map>
+    </div>
+  );
+};
+
+export default Mapbox;
