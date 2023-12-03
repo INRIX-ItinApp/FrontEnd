6a42f240572f21074112b84a90fdf312b58169e8
dimentio233 <johnnyhe7@outlook.com>
2023-12-03 08:46:56 -0800

    map data

--- my-app/src/components/Form.js
+++ my-app/src/components/Form.js
@@ -30,87 +30,6 @@ const FormPage = () => {
     setFields([...values]);
   };
 
-<<<<<<< HEAD
-  let navigate = useNavigate();
-  const routeChange = () => {
-    let path = "/Mapbox";
-    navigate(path);
-  };
-
-  return (
-    <div>
-      <Container>
-        <Row
-          style={{
-            fontWeight: "bold",
-            paddingLeft: "100px",
-            paddingRight: "100px",
-          }}
-        >
-          {/* Task List */}
-          {fields.length ? (
-            <Form className="no-bullet">
-              <Form.Group className="mb-3" controlId="formBasicEntry">
-                {fields.map((field, i) => (
-                  <div key={field.id}>
-                    <Row className="align-items-center mt-5">
-                      <Col md>
-                        {/* <Form.Label>First Task</Form.Label> */}
-                        <Form.Control
-                          type="text"
-                          placeholder="Enter an errand ..."
-                          name="firstErrand"
-                          value={field.firstErrand}
-                          onChange={(e) => handleChangeInput(i, e)}
-                        />
-                      </Col>
-                      <Col md>
-                        <Button
-                          disabled={fields.length === 4}
-                          onClick={() => handleAdd(i)}
-                          className="mt-4 mr-5"
-                          style={{ float: "center" }}
-                        >
-                          <i className="fas fa-plus"></i>
-                        </Button>
-                        <Button
-                          onClick={() => handleSubtract(i)}
-                          className="minus-button mt-4"
-                        >
-                          <i className="fas fa-minus"></i>
-                        </Button>
-                      </Col>
-                    </Row>
-                  </div>
-                ))}
-              </Form.Group>
-              <div style={{ float: "right", marginRight: "250px" }}>
-                <Button
-                  type="submit"
-                  variant="light"
-                  onClick={() => {
-                    routeChange();
-                  }}
-                >
-                  Submit
-                </Button>
-              </div>
-            </Form>
-          ) : (
-            <button
-              style={{ width: "250px", marginLeft: "353px", marginTop: "50px" }}
-              className="glow-on-hover"
-              onClick={() => handleAdd(0)}
-            >
-              Let's Get Started!
-            </button>
-          )}
-        </Row>
-      </Container>
-    </div>
-  );
-};
-=======
     return(
         <div>
             <Container>
@@ -163,6 +82,5 @@ const FormPage = () => {
         </div>
     )
 }
->>>>>>> ca17a309e3ce4f833bf8ae0f8db06a035dfd7da6
 
 export default FormPage;
