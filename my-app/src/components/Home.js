2bea6d548c75451ad381734a94abfe569c1d69e1
kyleung1 <leungkyle2@gmail.com>
2023-12-03 00:52:49 -0800

    styling

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -1,15 +1,26 @@
 import FormPage from "./Form";
 
 const Home = () => {
-    return(
-        <div className="Intro">
-          <h1>Welcome to Ease!</h1>
-          <p>With our AI tool, save time on daily errand running and plan your day with Ease.</p>
-          <p>We take the guesswork out of your route so you can get your tasks done faster and have more time for you!</p>
-          <h2>Your Day, Your Way!</h2>
-      <FormPage/>
+  return (
+    <div className="Intro">
+      <h1>
+        Welcome to <span className="highlight">Ease</span>!
+      </h1>
+      <p>
+        With our AI tool, save time on daily errand running and plan your day
+        with Ease.
+      </p>
+      <p>
+        We take the guesswork out of your route so you can get your tasks done
+        faster and have more time for you!
+      </p>
+      <h2>
+        Your <span className="highlight">Day</span>, Your{" "}
+        <span className="highlight">Way</span>!
+      </h2>
+      <FormPage />
     </div>
-    )
-}
+  );
+};
 
 export default Home;
